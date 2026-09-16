9020ef39919c11ed874039b9917a83de8b6b2a8c
jisoo <0801ljs@naver.com>
2026-09-16 15:22:13 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/ktb/lookddak/LookddakApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.lookddak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookddakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookddakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktb/lookddak/LookddakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.lookddak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookddakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
